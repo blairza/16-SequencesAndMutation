2714ee8d4d706fd7544674d38a4e169e98c984af
blairza <blairza@rose-hulman.edu>
2019-01-14 15:56:23 -0500

    DONE

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zane Blair.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -103,9 +103,14 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    place = strings[0]
+    for k in range(len(strings)):
+        if(len(strings[k]) < len(place)):
+            place = strings[k]
+    return place
 
 
 def run_test_index_of_largest_number():
@@ -136,15 +141,11 @@ def run_test_index_of_largest_number():
     print('Expected and actual are:', expected, answer)
 
     expected = 1
-    answer = index_of_largest_number([-500000000000000000000000000000,
-                                      - 400000000000000000000000000000],
-                                     2)
+    answer = index_of_largest_number([-500000000000000000000000000000,-400000000000000000000000000000],2)
     print('Expected and actual are:', expected, answer)
 
     expected = 0
-    answer = index_of_largest_number([-40000000000000000000000000000000000,
-                                      - 50000000000000000000000000000000000],
-                                     2)
+    answer = index_of_largest_number([-40000000000000000000000000000000000,-50000000000000000000000000000000000],2)
     print('Expected and actual are:', expected, answer)
 
 
@@ -180,9 +181,16 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    place = 0
+    place1 = -90000000000000000000000000000000000000000000000000000000000000
+    for k in range(n):
+        if(numbers[k] > place1):
+            place = k
+            place1 = numbers[k]
+    return place
 
 
 # ----------------------------------------------------------------------
@@ -235,9 +243,14 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(s) - 1):
+        if s[k + 1] == s[k]:
+            count = count + 1
+    return count
 
 
 def run_test_is_palindrome():
@@ -308,7 +321,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -317,6 +330,14 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(0,len(s)//2):
+        if(s[k] == s[len(s)-1-k]):
+            count += 1
+    if (count == len(s) // 2):
+        return True
+    else:
+        return False
 
 
 # ----------------------------------------------------------------------
@@ -375,9 +396,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(sequence1)-1):
+        if(sequence1[k] == sequence2[k]):
+            count += 1
+    return count
 
 
 # ----------------------------------------------------------------------
